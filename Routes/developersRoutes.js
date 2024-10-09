180802145ffae75596837b2fcb2afe19a3290163
joychandrauday <joychandraud@gmail.com>
2024-10-10 04:13:05 +0600

    fixing some issues

--- Routes/developersRoutes.js
+++ Routes/developersRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router()
-const {developers} = require("../models/developer")
+const express = require("express");
+const router = express.Router();
+const { developers } = require("../models/developer");
 
-router.get("/", async(req,res)=>{
-    try {
-        const result = await developers.find();
-        res.json(result);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const result = await developers.find();
+    res.send(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-module.exports = router
+module.exports = router;
